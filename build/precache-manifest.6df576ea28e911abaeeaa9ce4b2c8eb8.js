b1e68ee2fa5cbd64d3a0c70d52daf8af8df1565e
suyeon0103 <k_sy0103@naver.com>
2020-02-19 22:31:04 +0900

    image edit

---
+++ build/precache-manifest.6df576ea28e911abaeeaa9ce4b2c8eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a89317ca4a85f8394b2150b558fb8b",
+    "url": "/festival_de_cannes/index.html"
+  },
+  {
+    "revision": "1e9a8314ae286d7f9a98",
+    "url": "/festival_de_cannes/static/css/main.1273322f.chunk.css"
+  },
+  {
+    "revision": "d7dea0890caa8c5535f0",
+    "url": "/festival_de_cannes/static/js/2.59cefb43.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/festival_de_cannes/static/js/2.59cefb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9a8314ae286d7f9a98",
+    "url": "/festival_de_cannes/static/js/main.c84b841b.chunk.js"
+  },
+  {
+    "revision": "5d608e96cfc75b7d97a1",
+    "url": "/festival_de_cannes/static/js/runtime-main.69345819.js"
+  }
+]);
